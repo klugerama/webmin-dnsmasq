3d12a2aed7bb66f952069fdb50ef1add4604b1dc
Loren Cress <lcress@gmail.com>
2023-08-31 18:17:31 -0600

    Resolves #5

--- dnsmasq/acl_security.pl
+++ dnsmasq/acl_security.pl
@@ -34,14 +34,22 @@ sub acl_security_form {
         $_[0]->{'dump_logs'} ? 'checked' : '', $text{"yes"};
     printf "<input type=radio name=dump_logs value=0 %s> %s</td> </tr>\n",
         $_[0]->{'dump_logs'} ? '' : 'checked', $text{"no"};
+
+    print "<tr> <td><b>".$text{"acl_view_logs"}."</b></td>\n";
+    printf "<td><input type=radio name=view_logs value=1 %s> %s\n",
+        $_[0]->{'view_logs'} ? 'checked' : '', $text{"yes"};
+    printf "<input type=radio name=view_logs value=0 %s> %s</td> </tr>\n",
+        $_[0]->{'view_logs'} ? '' : 'checked', $text{"no"};
 }
 
 # acl_security_save(&options)
 # Parse the form for security options for the dnsmasq module
 sub acl_security_save {
     $_[0]->{'start'} = $in{'start'};
+    $_[0]->{'stop'} = $in{'stop'};
     $_[0]->{'restart'} = $in{'restart'};
     $_[0]->{'reload'} = $in{'reload'};
     $_[0]->{'dump_logs'} = $in{'dump_logs'};
+    $_[0]->{'view_logs'} = $in{'view_logs'};
 }
 
